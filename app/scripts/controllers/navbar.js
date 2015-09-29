6152a915caf8d09726d9894563c8c819d9392118
tieptoi <garrynguyen1990@gmail.com>
2015-09-29 17:54:56 +0800

    modified ui and fixed bugs

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -1,21 +1,22 @@
 'use strict';
 
 angular.module('todoApp')
-  .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
+    .controller('NavbarCtrl', function ($scope, $location) {
+        $scope.menu = [{
+            'title': 'Home',
+            'link': '/'
     }, {
-      'title': 'Items',
-      'link': '/items'
+            'title': 'Items',
+            'link': '/items'
     }, {
-      'title': 'Contact',
-      'link': '/contact'
+            'title': 'Contact',
+            'link': '/contact'
     }, {
-      'title': 'About',
-      'link': '/about'
+            'title': 'About',
+            'link': '/about'
     }];
-    $scope.isActive = function (route) {
-      return route === $location.path();
-    };
-  });
+        $scope.isActive = function (route) {
+            console.log('route: ' + route + 'path:' + $location.path());
+            return route === $location.path();
+        };
+    });
