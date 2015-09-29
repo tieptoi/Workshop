15a100e10c55979dca45adb1e9ae81f6b92d6fe3
tieptoi <garrynguyen1990@gmail.com>
2015-09-29 23:38:57 +0800

    Moved nav bar into index.html instead of keep it in every pages

--- app/scripts/controllers/item.js
+++ app/scripts/controllers/item.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('todoApp')
-    .controller('ItemCtrl', function ($scope, $log, ItemService) {
+    .controller('ItemCtrl', function ($scope, $cookies, $log, ItemService) {
         /// Properties===============
         $scope.items = [];
         $scope.item = {};
@@ -32,64 +32,10 @@ angular.module('todoApp')
             $scope.master = angular.copy($scope.item);
         };
 
-        /* Check There are any item in cart */
-        $scope.anyItem = function () {
-            var flag = ($scope.cart.items.length > 0) ? true : false;
-            return flag;
-        };
-
         /*  Add item to cart */
         $scope.add = function (item, form) {
-            if (form.$valid) {
-                //Check duplicate item in cart
-                console.log(form.$valid);
-                var dup = false;
-                angular.forEach($scope.cart.items, function (i) {
-                    if (i.name === item.name) {
-                        dup = true;
-                        console.log(i.orderQuantity);
-                        i.orderQuantity = i.orderQuantity + item.orderQuantity;
-                    }
-                });
-                if (!dup) {
-                    $log.info(item);
-                    /*Dont ever use this way to add item to array cause it link item in cart back to [$scope.item] */
+            $scope.$broadcast('add2Cart', item, form);
 
-                    //$scope.cart.items.push(item);
-
-                    /* Instead use this way */
-                    $scope.cart.items.push({
-                        description: item.description,
-                        image: item.image,
-                        quantity: item.quantity,
-                        name: item.name,
-                        pid: item.pid,
-                        price: item.price,
-                        orderQuantity: item.orderQuantity
-                    });
-                }
-                $scope.changeQuantity();
-            } else {
-                angular.copy($scope.master, $scope.item);
-            }
-        };
-
-        /* Remove item in cart */
-        $scope.remove = function (item) {
-            if (item.orderQuantity > 0) {
-                $scope.cart.total = $scope.cart.total - (item.orderQuantity * item.price)
-            }
-            $scope.cart.items.splice($scope.cart.items.indexOf(item), 1);
-        };
-
-        /* Change Cart Item Quantity */
-        $scope.changeQuantity = function () {
-            $scope.cart.total = 0;
-            angular.forEach($scope.cart.items, function (item) {
-                if (item.orderQuantity > 0) {
-                    $scope.cart.total = $scope.cart.total + (item.orderQuantity * item.price);
-                }
-            });
         };
 
     });
--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -1,7 +1,13 @@
 'use strict';
 
 angular.module('todoApp')
-    .controller('NavbarCtrl', function ($scope, $location) {
+    .controller('NavbarCtrl', function ($scope, $cookies, $location) {
+        //Properties
+        $scope.cart = {
+            items: [],
+            total: 0
+        };
+
         $scope.menu = [{
             'title': 'Home',
             'link': '/'
@@ -16,7 +22,80 @@ angular.module('todoApp')
             'link': '/about'
     }];
         $scope.isActive = function (route) {
-            console.log('route: ' + route + 'path:' + $location.path());
             return route === $location.path();
         };
+
+        /* Check There are any item in cart */
+        $scope.anyItem = function () {
+            var flag = ($scope.cart.items.length > 0) ? true : false;
+            return flag;
+        };
+
+        /* Add item to cart by listening on add2Cart event */
+        $scope.$on('add2Cart', function (event, item, form) {
+            if (form.$valid) {
+                //Check duplicate item in cart
+                //console.log(form.$valid);
+                var dup = false;
+                angular.forEach($scope.cart.items, function (i) {
+                    if (i.name === item.name) {
+                        dup = true;
+                        console.log(i.orderQuantity);
+                        i.orderQuantity = i.orderQuantity + item.orderQuantity;
+                    }
+                });
+                if (!dup) {
+                    //$log.info(item);
+                    /*Dont ever use this way to add item to array cause it link item in cart back to [$scope.item] */
+
+                    //$scope.cart.items.push(item);
+
+                    /* Instead use this way */
+                    $scope.cart.items.push({
+                        description: item.description,
+                        image: item.image,
+                        quantity: item.quantity,
+                        name: item.name,
+                        pid: item.pid,
+                        price: item.price,
+                        orderQuantity: item.orderQuantity
+                    });
+
+                    // Setting a cookie
+                    $cookies.putObject('cart', $scope.cart);
+                }
+                $scope.changeQuantity();
+            } else {
+                angular.copy($scope.master, $scope.item);
+            }
+        });
+
+        /* Remove item in cart */
+        $scope.remove = function (item) {
+            if (item.orderQuantity > 0) {
+                $scope.cart.total = $scope.cart.total - (item.orderQuantity * item.price)
+            }
+            $scope.cart.items.splice($scope.cart.items.indexOf(item), 1);
+            // Setting a cookie
+            $cookies.putObject('cart', $scope.cart);
+
+        };
+
+        /* Change Cart Item Quantity */
+        $scope.changeQuantity = function () {
+            $scope.cart.total = 0;
+            angular.forEach($scope.cart.items, function (item) {
+                if (item.orderQuantity > 0) {
+                    $scope.cart.total = $scope.cart.total + (item.orderQuantity * item.price);
+                }
+            });
+        };
+
+
+        // Retrieving a cookie
+        if ($cookies.getObject('cart') != null) {
+            $scope.cart = $cookies.getObject('cart');
+            $scope.changeQuantity();
+        }
+
     });
